5636ca2f1144b0a300b5503d1b5926e8c439c11a
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-01 00:14:57 +0300

    Creating Mongoose Models

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected!"))
+  .catch(console.log);
 
 const server = new ApolloServer({
   typeDefs,
--- typeDefs.js
+++ typeDefs.js
@@ -8,8 +8,27 @@ module.exports = gql`
     picture: String
   }
 
+  type Pin {
+    _id: ID
+    createdAt: String
+    title: String
+    content: String
+    image: String
+    latitude: Float
+    longitude: Float
+    author: User
+    comments: [Comment]
+  }
+
+  type Comment {
+    text: String
+    createdAt: String
+    author: User
+  }
+
   type Query {
     me: User
+    pin: Pin
   }
 `;
 
