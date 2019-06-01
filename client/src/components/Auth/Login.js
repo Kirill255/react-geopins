c16277e2c98fa6048e9f3b821a9c7355d750b71f
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-01 12:04:40 +0300

    Managing App State with useContext / useReducer

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { GraphQLClient } from "graphql-request";
 import { GoogleLogin } from "react-google-login";
 import { withStyles } from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
+
+import Context from "../../context";
 
 const ME_QUERY = `
 {
@@ -17,15 +19,18 @@ const ME_QUERY = `
 
 // 1. Получаем токен, отсылаем на сервер
 const Login = ({ classes }) => {
+  const { dispatch } = useContext(Context);
+
   const onSuccess = async (googleUser) => {
-    console.log({ googleUser });
+    // console.log({ googleUser });
     const idToken = googleUser.getAuthResponse().id_token;
-    console.log({ idToken });
+    // console.log({ idToken });
     const client = new GraphQLClient("http://localhost:4000/graphql", {
       headers: { authorization: idToken }
     });
     const data = await client.request(ME_QUERY);
-    console.log({ data });
+    // console.log({ data });
+    dispatch({ type: "LOGIN_USER", payload: data.me });
   };
 
   return (
