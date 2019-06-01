2c124d295f6f013e2302c69b7f7d8d07d56baaca
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-06-01 20:02:11 +0300

    Creating and Styling our Map

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactMapGL, { NavigationControl } from "react-map-gl";
 import { withStyles } from "@material-ui/core/styles";
 // import Button from "@material-ui/core/Button";
 // import Typography from "@material-ui/core/Typography";
 // import DeleteIcon from "@material-ui/icons/DeleteTwoTone";
 
+const INITIAL_VIEWPORT = {
+  latitude: 37.7577,
+  longitude: -122.4376,
+  zoom: 13
+};
+
 const Map = ({ classes }) => {
-  return <div>Map</div>; // <div className={classes.popupImage}>Map</div>
+  const [viewport, setViewport] = useState(INITIAL_VIEWPORT);
+
+  return (
+    <div className={classes.root}>
+      <ReactMapGL
+        mapboxApiAccessToken="pk.eyJ1Ijoia2lyaWxsMjU1IiwiYSI6ImNqd2RwcmRoajE1ZTE0YW1xZ3l6cnFlamcifQ.8qNdoa1oOewuiFMSt0kr-w"
+        width="100vw"
+        height="calc(100vh - 64px)"
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        onViewportChange={(newViewport) => setViewport(newViewport)}
+        {...viewport}
+      >
+        {/* navigation control */}
+        <div className={classes.navigationControl}>
+          <NavigationControl onViewportChange={(newViewport) => setViewport(newViewport)} />
+        </div>
+      </ReactMapGL>
+    </div>
+  );
 };
 
 // const styles = (theme) => ({}); // or just object const styles = {};
@@ -41,3 +66,7 @@ const styles = {
 };
 
 export default withStyles(styles)(Map);
+
+/*
+https://docs.mapbox.com/mapbox-gl-js/example/setstyle/
+*/
